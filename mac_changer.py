7fb4cc3e3918f5e24beb8056e7f9b2c5e7eea0ca
AZARADO <145587412+azarado-006@users.noreply.github.com>
2024-04-29 06:06:34 -0400

    Add files via upload
    custom Mac address changer

---
+++ mac_changer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python
+
+interface=input("interface:>(eth0/wlan0)")
+newmac=input("new mac:>")
+
+import subprocess
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", newmac])
+subprocess.call(["ifconfig", interface, "up"])
+subprocess.call(["ifconfig"])
